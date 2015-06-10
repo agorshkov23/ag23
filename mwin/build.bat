e05591d1275cf51f73fd94bad7ca79c7a97c9844
Alexey Gorshkov <agorshkov23@yandex.ru>
2015-06-10 21:04:04 +0300

    added new hotkeys

--- mwin/build.bat
+++ mwin/build.bat
@@ -1,3 +1,4 @@
+@echo off
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 cl /EHsc mwin.cpp User32.lib
--- mwin/mwin.cpp
+++ mwin/mwin.cpp
@@ -4,30 +4,32 @@ using namespace std;
 
 #define	VK_S	83
 #define	VK_H	72
+#define VK_T	84
 
 HWND hWnd = GetConsoleWindow();
 
-void hide() {
-	ShowWindow(hWnd, SW_HIDE);
-}
-
-void show() {
-	ShowWindow(hWnd, SW_SHOW);
+void ToggleWindow() {
+	if (IsWindowVisible (hWnd))
+		ShowWindow(hWnd, SW_HIDE);
+	else
+		ShowWindow(hWnd, SW_SHOW);
 }
 
 void init() {
+	BOOL bError = FALSE;
 	cout << "mwindow registering hotkeys..." << endl;
 
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN, VK_UP))						cout << "error registering hotkey win+alt+up" << endl;
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN, VK_DOWN))					cout << "error registering hotkey win+alt+down" << endl;
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_F8))		cout << "error registering hotkey win+alt+f8" << endl;
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_LEFT))		cout << "error registering hotkey win+alt+left" << endl;
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_RIGHT))	cout << "error registering hotkey win+alt+right" << endl;
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_SPACE))	cout << "error registering hotkey win+alt+space" << endl;
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_F6))		cout << "error registering hotkey win+alt+f6" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN, VK_UP))						cout << "error registering hotkey win+alt+up" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN, VK_DOWN))					cout << "error registering hotkey win+alt+down" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_F8))		cout << "error registering hotkey win+alt+f8" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_LEFT))	cout << "error registering hotkey win+alt+left" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_RIGHT))	cout << "error registering hotkey win+alt+right" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_SPACE))	cout << "error registering hotkey win+alt+space" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_F6))		cout << "error registering hotkey win+alt+f6" << endl;
 	
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_H))		cout << "error registering hotkey win+alt+h" << endl;
-	if (!RegisterHotKey(NULL, NULL, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_S))		cout << "error registering hotkey win+alt+s" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_T))		cout << "error registering hotkey win+alt+t" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_H))		cout << "error registering hotkey win+alt+h" << endl;
+	if (!RegisterHotKey(NULL, WM_HOTKEY, MOD_ALT | MOD_WIN | MOD_NOREPEAT, VK_S))		cout << "error registering hotkey win+alt+s" << endl;
 	
 }
 
@@ -40,39 +42,36 @@ void loop() {
 			{
 			case MOD_ALT | MOD_WIN | VK_UP << 16:
 				keybd_event(VK_VOLUME_UP, NULL, NULL, NULL);
-				cout << "up" << endl;
 				break;
 			case MOD_ALT | MOD_WIN | VK_DOWN << 16:
 				keybd_event(VK_VOLUME_DOWN, NULL, NULL, NULL);
-				cout << "down" << endl;
 				break;
 			case MOD_ALT | MOD_WIN | VK_F8 << 16:
 				keybd_event(VK_VOLUME_MUTE, NULL, NULL, NULL);
-				cout << "mute" << endl;
 				break;
 			case MOD_ALT | MOD_WIN | VK_LEFT << 16:
 				keybd_event(VK_MEDIA_PREV_TRACK, NULL, NULL, NULL);
-				cout << "prev" << endl;
 				break;
 			case MOD_ALT | MOD_WIN | VK_RIGHT << 16:
 				keybd_event(VK_MEDIA_NEXT_TRACK, NULL, NULL, NULL);
-				cout << "next" << endl;
 				break;
 			case MOD_ALT | MOD_WIN | VK_SPACE << 16:
 				keybd_event(VK_MEDIA_PLAY_PAUSE, NULL, NULL, NULL);
-				cout << "pause" << endl;
 				break;
 			case MOD_ALT | MOD_WIN | VK_F6 << 16: 
 				SendMessage(FindWindow(NULL, NULL), WM_SYSCOMMAND, SC_MONITORPOWER, 2);
 				break;
 			case MOD_ALT | MOD_WIN | VK_H << 16:
-				hide();
+				ShowWindow (hWnd, SW_HIDE);
 				break;
 			case MOD_ALT | MOD_WIN | VK_S << 16:
-				show();
+				ShowWindow (hWnd, SW_SHOW);
+				break;
+			case MOD_ALT | MOD_WIN | VK_T << 16:
+				ToggleWindow();
 				break;
 			default:
-				printf("default %x\n", msg.lParam);
+				printf("default (%d) 0x%x\n", msg.lParam, msg.lParam);
 				break;
 			}
 		}
